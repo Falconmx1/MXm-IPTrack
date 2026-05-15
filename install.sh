5feaba5375da5d38f63d69703f0ba6970f77ee26
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-15 00:04:35 -0600

    Add installation script for MXm-IPTrack

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "\033[0;32m[+] Instalando MXm-IPTrack...\033[0m"
+chmod +x mxtrack
+sudo cp mxtrack /usr/local/bin/
+echo -e "\033[0;32m[+] Listo. Escribe 'mxtrack' para comenzar.\033[0m"
